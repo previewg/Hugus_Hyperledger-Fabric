081ccdd061b1554d2a877fc275fe4ebd4d00a29f
previewg <preview6@gmail.com>
2020-09-12 17:05:10 +0900

    first setting

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require('cookie-parser')();
+require('morgan')('dev');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//app,js
+
+app.listen(process.env.PORT,console.log(`${process.env.PORT} port is listening...`));
+
 module.exports = app;
