337dee85e5f8e9b6bf105c7fb107f14489e0e60c
previewg <preview6@gmail.com>
2020-09-14 13:07:11 +0900

    sequelize init

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use((req,res,next)=>{
+	const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+	error.status = 404;
+	next(error);
+});
+
 app.listen(process.env.PORT,()=> console.log(`${process.env.PORT} port is listening...`));
 
 module.exports = app;
