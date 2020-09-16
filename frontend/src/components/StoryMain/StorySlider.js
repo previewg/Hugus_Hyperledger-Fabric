e72832862bd6cdb92f672604bef437421ed31899
previewg <preview6@gmail.com>
2020-09-16 15:22:59 +0900

    StoryMain header added

--- frontend/src/components/StoryMain/StorySlider.js
+++ frontend/src/components/StoryMain/StorySlider.js
@@ -11,8 +11,6 @@ const SliderStyle = styled.div`
       width: 90%;
     }
   }
-  
-  
 `;
 
 const StorySlider = () => {
--- frontend/src/containers/StoryMain.js
+++ frontend/src/containers/StoryMain.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import StorySlider from "../components/StoryMain/StorySlider";
+import styled from 'styled-components';
+
+const StoryMenuStyle = styled.div`
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  section{
+      width: 40%;
+      height: 50px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      article{
+        font-size: 20px;
+      }
+  }
+  
+`;
+
+
 
 const StoryMain = () => {
     return (
         <>
+            <StoryMenuStyle>
+                <section>
+                    <article>최신 스토리</article>
+                    <article>인기 스토리</article>
+                    <article>관심 스토리</article>
+                    <article>지난 스토리</article>
+                </section>
+            </StoryMenuStyle>
             <StorySlider />
         </>
     );
