8b145116b789fad78b562d90cf733d07b9969d0a
moonnr94 <moonnr94@gmail.com>
2020-09-17 11:07:56 +0900

    수정

--- frontend/src/containers/StoryWrite.js
+++ frontend/src/containers/StoryWrite.js
@@ -5,43 +5,37 @@ import axios from "axios";
 const StoryWriteStyle = styled.div`
             display:flex;
             justify-content:center;
-            margin-top:140px;
+            margin-top:60px;
             margin-bottom:70px;
-            .layout {
-            width:60%;
-            display:flex;
-            flex-direction: column;
-            align-items: flex-start;
-
-            
-            .write_title {
-            position:relative;
-            left:33%;
-            text-align:center;
-            width:260px;
-            padding:10px;
-            border-bottom:solid orange;
-
-            }
-        
-
-            .title {
-            margin-top:50px;
-            display:flex;
-            align-items:center;
-                p{
-                margin-right:50px;
+                .layout {
+                width:1000px;
+                display:flex;
+                flex-direction: column;
+                align-items: center;
+                .write_title {
+                position:relative;
+                text-align:center;
+                width:260px;
+                padding:10px;
+                border-bottom:solid orange;
                 }
-                input {
-                border:none;
-                width:300px;
-                padding:5px;
-                border-bottom:solid 0.1px gray;
-                :focus{
-                    outline:none;
+                .title {
+                margin-top:50px;
+                display:flex;
+                align-items:center;
+                    p{
+                    margin-right:50px;
+                    }
+                    input {
+                    border:none;
+                    width:300px;
+                    padding:5px;
+                    border-bottom:solid 0.1px gray;
+                    :focus{
+                        outline:none;
+                    }
+                    }
                 }
-                }
-            }
 
             .info{
             margin-top:35px;
@@ -65,30 +59,30 @@ const StoryWriteStyle = styled.div`
                 width:100%;
                 display:flex;
                 justify-content:space-between;
-                
+                align-items:flex-end;
                 }
                 div:nth-child(2){
                 display:flex;
-                width:22%;
-                    p {
-                    font-size:70%;
+                width:15%;
+                justify-content:flex-end;
+                align-items: center;
+                    p:nth-child(1) {
+                    font-size:80%;
                     }
+                    p:nth-child(2) {
+                    display:flex;
+                    justify-content:space-between;
                     img {
-                        display:flex;
-                justify-content:space-between;
-                    width:28px;
-                    height:28px;
+                    width:22px;
+                    height:22px;
+                    } 
+                    }
                 }
-
-                }
-                
                 textarea {
                 padding-top:12px;
                 padding-left:12px;
             }
         }
-
-
             .item {
                 margin-top:35px;
                 textarea {
@@ -102,26 +96,30 @@ const StoryWriteStyle = styled.div`
             }
             .hashtag {
                 margin-top:20px;
-
                 textarea {
                 padding-top:12px;
                 padding-left:12px;
                 border:none;
-
                 }
             }
-
-            .button {
+            .submit {
+            width:100%;
+            display:flex;
+            justify-content:flex-end;
+            align-items:center;
+            button {
                 margin-top:15px;
                 border:none;
                 background:white;
-                width:175%;
+                font-size:large;
+                }
+            img {
+                margin-top:15px;
+                width:28px;
+                height:28px;    
+            }    
             }
-
-
             }
-
-            
         `;
 
 const StoryWrite = () => {
@@ -154,11 +152,11 @@ const StoryWrite = () => {
                         <p>내용</p>
                         <div>
                         <p>파일 첨부</p>
-                        <div className="imgs">
+                            <p>
                         <img className="clip" src="/icons/Clip.png"/>
-                        <img className="picture" src="/icons/Picture.png"/>
-                        <img className="video" src="/icons/Video.png"/>
-                        </div>                      
+                        {/* <img className="picture" src="/icons/Picture.png"/>
+                        <img className="video" src="/icons/Video.png"/> */}
+                            </p>                      
                         </div>
                     </div>
 
@@ -181,11 +179,36 @@ const StoryWrite = () => {
                     placeholder="#태그입력" class="form-control">
                     </textarea>
                 </div>
+                
 
-            <button className="button" onClick="">
-            제출하기
-            </button>
 
+
+                <body>
+    <div class="content">
+        <form action="writeDo.php" method="POST" id="tag-form">
+            <input type="hidden" value="" name="tag" id="rdTag" />
+            <button type="submit">태그등록</button>
+        </form>
+
+        <ul id="tag-list">
+        </ul>
+
+        <div>
+            <input type="text" id="tag" size="7" placeholder="태그입력" />
+        </div>
+
+    </div>
+</body>
+
+
+
+
+                <div className="submit">
+                        <button className="button" onClick="">
+                        제출하기
+                        </button>
+                        <img src="/icons/PaperPlane.png"/>
+                </div>
             </div>
         </StoryWriteStyle>
     );
