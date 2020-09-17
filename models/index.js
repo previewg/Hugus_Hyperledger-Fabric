4972a980e28177e586e30bba373d552791d0d2d6
previewg <preview6@gmail.com>
2020-09-17 14:47:01 +0900

    Story, User, campaign, like, comment DB joined

--- models/index.js
+++ models/index.js
@@ -13,6 +13,30 @@ db.sequelize = sequelize;
 
 db.User = require('./user')(sequelize, Sequelize);
 db.Story = require('./story')(sequelize, Sequelize);
+db.Comment = require('./comment')(sequelize, Sequelize);
+db.Like = require('./like')(sequelize, Sequelize);
+db.Campaign = require('./campaign')(sequelize, Sequelize);
+
+db.User.hasMany(db.Story,{foreignKey:'user_email',sourceKey:'email'});
+db.User.hasMany(db.Comment,{foreignKey:'user_email',sourceKey:'email'});
+db.User.hasMany(db.Campaign,{foreignKey:'user_email',sourceKey:'email'});
+
+db.Story.hasMany(db.Comment,{foreignKey:'story_id',sourceKey:'id'});
+db.Story.hasMany(db.Like,{foreignKey:'story_id',sourceKey:'id'});
+db.Story.belongsTo(db.User,{foreignKey:'user_email',targetKey:'email'});
+
+db.Comment.belongsTo(db.Story,{foreignKey:'story_id',targetKey:'id'});
+db.Comment.belongsTo(db.User,{foreignKey:'user_email',targetKey:'email'});
+db.Comment.belongsTo(db.Campaign,{foreignKey:'campaign_id',targetKey:'id'});
+
+db.Like.belongsTo(db.Story,{foreignKey:'story_id',targetKey:'id'});
+db.Like.belongsTo(db.User,{foreignKey:'user_email',targetKey:'email'});
+db.Like.belongsTo(db.Campaign,{foreignKey:'campaign_id',targetKey:'id'});
+
+db.Campaign.hasMany(db.Comment,{foreignKey:'campaign_id',sourceKey:'id'});
+db.Campaign.hasMany(db.Like,{foreignKey:'campaign_id',sourceKey:'id'});
+db.Campaign.belongsTo(db.User,{foreignKey:'user_email',targetKey:'email'});
+
 
 module.exports = db;
 
