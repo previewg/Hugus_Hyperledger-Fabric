9ee07fe76f95a6469726c1e8db8067ea5e57f699
RACEJB <>
2020-09-17 09:28:58 +0900

    storywrite modified

--- frontend/src/containers/StoryWrite.js
+++ frontend/src/containers/StoryWrite.js
@@ -1,20 +1,127 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import axios from "axios";
 
 const StoryWriteStyle = styled.div`
             display:flex;
+            justify-content:center;
+            margin-top:140px;
+            margin-bottom:70px;
+            .layout {
+            width:60%;
+            display:flex;
             flex-direction: column;
-            align-items: center;
-            .title{
+            align-items: flex-start;
+
+            
+            .write_title {
+            position:relative;
+            left:33%;
+            text-align:center;
+            width:260px;
+            padding:10px;
+            border-bottom:solid orange;
+
+            }
+        
+
+            .title {
+            margin-top:50px;
+            display:flex;
+            align-items:center;
+                p{
+                margin-right:50px;
+                }
+                input {
+                border:none;
+                width:300px;
+                padding:5px;
+                border-bottom:solid 0.1px gray;
+                :focus{
+                    outline:none;
+                }
+                }
             }
 
             .info{
+            margin-top:35px;
 
+                p{
+                
+                }
+                textarea {
+                padding-top:12px;
+                padding-left:12px;
+                    
+                }
             }
+
+
             .content{
+                margin-top:50px;
+                display:flex;
+                flex-direction:column;
+                div:nth-child(1){
+                width:100%;
+                display:flex;
+                justify-content:space-between;
+                
+                }
+                div:nth-child(2){
+                display:flex;
+                width:22%;
+                    p {
+                    font-size:70%;
+                    }
+                    img {
+                        display:flex;
+                justify-content:space-between;
+                    width:28px;
+                    height:28px;
+                }
+
+                }
+                
+                textarea {
+                padding-top:12px;
+                padding-left:12px;
+            }
+        }
+
+
+            .item {
+                margin-top:35px;
+                textarea {
+                    border:solid 2px orange;
+                    padding-top:12px;
+                    padding-left:12px;
+                    :focus{
+                    outline:none;
+                }
+                }
+            }
+            .hashtag {
+                margin-top:20px;
+
+                textarea {
+                padding-top:12px;
+                padding-left:12px;
+                border:none;
+
+                }
+            }
+
+            .button {
+                margin-top:15px;
+                border:none;
+                background:white;
+                width:175%;
+            }
+
 
             }
+
+            
         `;
 
 const StoryWrite = () => {
@@ -26,39 +133,59 @@ const StoryWrite = () => {
 
     return (
         <StoryWriteStyle>
-            <h4>글쓰기</h4>
-            <div className='title'>
-                <h5>제목</h5>
-                <td><input type="text" placeholder="제목을 입력하세요. " name="story_title" class="form-control"/></td>
-            </div>
+            <div className="layout">
+                <div className="write_title">글쓰기</div>
+                <div className='title'>
+                <p>제목</p>
+                <input type="text" placeholder="제목을 입력하세요." name="story_title" class="form-control" />
+                </div>
 
-            <div className="info">
-                <h5>작성자 소개</h5>
-                <td><textarea onkeydown="resize(this)" rows="7" cols="100"
-                              placeholder="본인을 마음껏 표현해주세요." class="form-control"></textarea></td>
-            </div>
 
-            <div className="content">
-                <h5>내용</h5>
-                <tr>
-                    <th>첨부파일:</th>
-                    <td><input type="text" placeholder="파일을 선택하세요. " name="filename"/></td>
-                </tr>
+                <div className="info">
+                    <p>작성자 소개</p>
+                    <textarea rows="7" cols="100"
+                    placeholder="본인을 마음껏 표현해주세요." class="form-control">
+                    </textarea>
+                </div>
 
-                <td><textarea onkeydown="resize(this)" rows="18" cols="100"
-                              class="form-control" placeholder="내용을 입력하세요. "></textarea></td>
-            </div>
 
-            <div className="item">
-                <h5>필요 물품</h5>
-                <td><textarea onkeydown="resize(this)" rows="7" cols="100"
-                              placeholder="# 물품입력" class="form-control"></textarea></td>
-            </div>
+                <div className="content">
+                    <div>
+                        <p>내용</p>
+                        <div>
+                        <p>파일 첨부</p>
+                        <div className="imgs">
+                        <img className="clip" src="/icons/Clip.png"/>
+                        <img className="picture" src="/icons/Picture.png"/>
+                        <img className="video" src="/icons/Video.png"/>
+                        </div>                      
+                        </div>
+                    </div>
 
-            <div className="hashtag">
-                <h5>태그</h5>
-                <td><textarea onkeydown="resize(this)" rows="7" cols="100"
-                              placeholder="#태그" class="form-control"></textarea></td>
+                    <textarea rows="18" cols="100"
+                    class="form-control" placeholder="내용을 입력하세요. ">
+                    </textarea>
+                </div>
+
+
+                <div className="item">
+                    <p>필요 물품</p>
+                    <textarea rows="7" cols="100"
+                    placeholder="# 물품입력" class="form-control"> 
+                    </textarea>
+                </div>
+
+                <div className="hashtag">
+                    <p>태그</p>
+                    <textarea rows="7" cols="100"
+                    placeholder="#태그입력" class="form-control">
+                    </textarea>
+                </div>
+
+            <button className="button" onClick="">
+            제출하기
+            </button>
+
             </div>
         </StoryWriteStyle>
     );
--- routes/story.js
+++ routes/story.js
@@ -2,17 +2,77 @@
 
 const express = require('express');
 const router = express.Router();
-const {Story} = require('../models');
+const { Story } = require('../models');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Story.findAll({})
-    .then((result)=>
-    console.log(result))
+    .then((result) =>
+      console.log(result))
 });
 
-router.post("/write",(req,res,next)=>{
-  res.json({res:req.body.ex});
-})
+// router.post("/getStoryList", async (req, res) => {
+//   try {
+//     const id = req.body.id;
+//     await Story.findAll({
+//       sort: { createdAt: -1 }
+//     })
+//     .then((res) => {
+//       res.json({ list: Story })
+//     })
+//   } catch (err) {
+//     console.log(err);
+//     res.json({ message: false });
+//   }
+// });
+
+router.post("/write", async (req, res) => {
+  try {
+    let obj;
+    obj = {
+      story_title: req.body.title,
+      user_info: req.body.user_info,
+      story_content: req.body.content,
+      story_hashtag: req.body.hashtag
+    };
+    const story = new Story(obj);
+
+    await story.save();
+    res.json({ message: "스토리가 업로드 되었습니다." });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
+router.post("/delete", async (req, res) => {
+  try {
+    await Story.destroy({where:{ id:req.body.id }});
+    res.json({ message: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
+router.post("/update", async (req, res) => {
+  try {
+    
+    await Story.update({
+      story_title: req.body.title,
+      user_info: req.body.user_info,
+      story_content: req.body.content,
+      story_hashtag: req.body.hashtag
+    },
+    {where: { id: req.body.id }
+});
+    res.json({ message: "게시글이 수정 되었습니다." });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
+
+
 
 module.exports = router;
