9e594eefa5f06f89cbf7eacf2d20bf77095381f6
RACEJB <moonnr94@gmail.com>
2020-10-27 10:25:28 +0900

    병합

--- db.js
+++ db.js
@@ -2,23 +2,6 @@ const mongoose = require("mongoose");
 let database;
 
 module.exports = () => {
-<<<<<<< HEAD
-    function connect() {
-        mongoose.connect('mongodb://localhost:27017/HugusBlock',
-            {useNewUrlParser: true, useUnifiedTopology: true},
-            function (err) {
-                if (err) {
-                    console.error('mongodb connection error', err);
-                }
-                console.log('mongoDB Connected');
-            }
-        )
-    }
-    connect();
-    mongoose.connection.on('disconnected', connect);
-    return database;
-}
-=======
   function connect() {
     mongoose.connect(
       "mongodb+srv://brave-son:qwe123@hugus0.hyowm.mongodb.net/HUGUS",
@@ -34,4 +17,3 @@ module.exports = () => {
   mongoose.connection.on("disconnected", connect);
   return database;
 };
->>>>>>> e41752f1d3397b6c2a5d519f60c3fb12b9b6fd5c
