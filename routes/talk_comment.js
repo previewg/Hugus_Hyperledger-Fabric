3ccfd148a4fcef4a59692cba2c0cd4ea2987c637
previewg <preview6@gmail.com>
2020-11-17 15:22:13 +0900

    11_17 finish

--- routes/talk_comment.js
+++ routes/talk_comment.js
@@ -10,258 +10,247 @@ const {
   sequelize,
 } = require("../models");
 
-
 // Talk 댓글 등록
 router.post("/add", async (req, res) => {
-    try {
-        const {user_email} = req.session.loginInfo;
-        const {talk_id, comment} = req.body;
-        if (req.body.type === "mobile") {
-            await Talk_Comment.create({
-                user_email: req.body.email,
-                talk_id: req.body.talk_id,
-                comment: req.body.comment,
-            })
-        }else{
-            await Talk_Comment.create({
-                user_email,
-                talk_id,
-                comment,
-            });
-        }
-
-
-        const list = await Talk_Comment.findAll({
-            attributes: [
-                "id",
-                "user_email",
-                "comment",
-                "createdAt",
-                [
-                    sequelize.literal(
-                        "(SELECT COUNT(comment_id) FROM talk_comment_child WHERE comment_id = `Talk_Comment`.id)"
-                    ),
-                    "child_count",
-                ],
-            ],
-            where: {
-                talk_id: req.body.talk_id,
-            },
-            order: [["created_at", "DESC"]],
-            include: [{model: User, attributes: ["nickname", "user_profile"]}],
-            limit: 10,
-        });
-
-        const total = await Talk_Comment.count({
-            where: {
-                talk_id: talk_id,
-            },
-        });
-
-        let more = false;
-        if (total > 10) more = true;
-
-        res.json({list: list, success: 1, total: total, more: more});
-    } catch (error) {
-      console.error(error);
-      res.status(400).json({ success: 3 });
+  try {
+    const { user_email } = req.session.loginInfo;
+    const { talk_id, comment } = req.body;
+    if (req.body.type === "mobile") {
+      await Talk_Comment.create({
+        user_email: req.body.email,
+        talk_id: req.body.talk_id,
+        comment: req.body.comment,
+      });
+    } else {
+      await Talk_Comment.create({
+        user_email,
+        talk_id,
+        comment,
+      });
     }
-  });
 
+    const list = await Talk_Comment.findAll({
+      attributes: [
+        "id",
+        "user_email",
+        "comment",
+        "createdAt",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(comment_id) FROM talk_comment_child WHERE comment_id = `Talk_Comment`.id)"
+          ),
+          "child_count",
+        ],
+      ],
+      where: {
+        talk_id: req.body.talk_id,
+      },
+      order: [["created_at", "DESC"]],
+      include: [{ model: User, attributes: ["nickname", "user_profile"] }],
+      limit: 10,
+    });
+
+    const total = await Talk_Comment.count({
+      where: {
+        talk_id: talk_id,
+      },
+    });
+
+    let more = false;
+    if (total > 10) more = true;
+
+    res.json({ list: list, success: 1, total: total, more: more });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ success: 3 });
+  }
+});
 
 // Talk 댓글 삭제
 router.post("/delete", async (req, res) => {
-    const { talk_id, comment_id } = req.body;
+  const { talk_id, comment_id } = req.body;
 
-    try {
-      await Talk_Comment.destroy({
-        where: {
-          id: comment_id,
-        },
-      });
-      await Talk_Comment_Child.destroy({
-        where: {
-          id: comment_id,
-        },
-      });
+  try {
+    await Talk_Comment.destroy({
+      where: {
+        id: comment_id,
+      },
+    });
+    await Talk_Comment_Child.destroy({
+      where: {
+        id: comment_id,
+      },
+    });
 
-      const list = await Talk_Comment.findAll({
-        attributes: [
-          "id",
-          "user_email",
-          "comment",
-          "createdAt",
-          [
-            sequelize.literal(
-              "(SELECT COUNT(comment_id) FROM comment_child WHERE comment_id = `Talk_Comment`.id)"
-            ),
-            "child_count",
-          ],
+    const list = await Talk_Comment.findAll({
+      attributes: [
+        "id",
+        "user_email",
+        "comment",
+        "createdAt",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(comment_id) FROM comment_child WHERE comment_id = `Talk_Comment`.id)"
+          ),
+          "child_count",
         ],
-        where: {
-          talk_id,
-        },
-        order: [["created_at", "DESC"]],
-        include: [{ model: User, attributes: ["nickname", "user_profile"] }],
-        limit: 10,
-      });
+      ],
+      where: {
+        talk_id,
+      },
+      order: [["created_at", "DESC"]],
+      include: [{ model: User, attributes: ["nickname", "user_profile"] }],
+      limit: 10,
+    });
 
-      const total = await Talk_Comment.count({
-        where: {
-          talk_id: talk_id,
-        },
-      });
+    const total = await Talk_Comment.count({
+      where: {
+        talk_id: talk_id,
+      },
+    });
 
-      let more = false;
-      if (total > 10) more = true;
+    let more = false;
+    if (total > 10) more = true;
 
-      res.json({ list: list, success: 1, total: total, more: more });
-    } catch (err) {
-      console.log(err);
-      res.json({ message: false });
-    }
-  });
+    res.json({ list: list, success: 1, total: total, more: more });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
 
-  // Talk 댓글 목록 조회
+// Talk 댓글 목록 조회
 router.get("/list/:id/:page", async (req, res) => {
-    try {
-      let talk_id = req.params.id;
-      let page = req.params.page;
-      let offset = 0;
+  try {
+    let talk_id = req.params.id;
+    let page = req.params.page;
+    let offset = 0;
 
-      // 10개씩 조회
-      if (page > 1) {
-        offset = 10 * (page - 1);
-      }
-      const list = await Talk_Comment.findAll({
-        attributes: [
-          "id",
-          "user_email",
-          "comment",
-          "createdAt",
-          [
-            sequelize.literal(
-              "(SELECT COUNT(comment_id) FROM talk_comment_child WHERE comment_id = `Talk_Comment`.id)"
-            ),
-            "child_count",
-          ],
-        ],
-        include: [
-          { model: User, attributes: ["nickname", "user_profile"] },
-        ],
-
-        where: {
-          talk_id: talk_id,
-        },
-        order: [["created_at", "DESC"]],
-        offset: offset,
-        limit: 10,
-      });
-
-      const total = await Talk_Comment.count({
-        where: {
-          talk_id: talk_id,
-        },
-      });
-
-      let more = false;
-      if (total > offset + 10) more = true;
-
-      res.json({ list: list, success: 1, more: more, total: total });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ success: 3 });
+    // 10개씩 조회
+    if (page > 1) {
+      offset = 10 * (page - 1);
     }
-  });
+    const list = await Talk_Comment.findAll({
+      attributes: [
+        "id",
+        "user_email",
+        "comment",
+        "createdAt",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(comment_id) FROM talk_comment_child WHERE comment_id = `Talk_Comment`.id)"
+          ),
+          "child_count",
+        ],
+      ],
+      include: [{ model: User, attributes: ["nickname", "user_profile"] }],
+
+      where: {
+        talk_id: talk_id,
+      },
+      order: [["created_at", "DESC"]],
+      offset: offset,
+      limit: 10,
+    });
+
+    const total = await Talk_Comment.count({
+      where: {
+        talk_id: talk_id,
+      },
+    });
+
+    let more = false;
+    if (total > offset + 10) more = true;
+
+    res.json({ list: list, success: 1, more: more, total: total });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: 3 });
+  }
+});
 
 // Talk 대댓글 작성
 router.post("/child/add", async (req, res) => {
-    try {
-      const user_email = req.session.loginInfo.user_email;
-      // const user_email = "moonnr94@gmail.com";
-      const { comment_id, comment, talk_id } = req.body;
-      await Talk_Comment_Child.create({
-        user_email: user_email,
-        comment_id: comment_id,
-        comment: comment,
-      });
-      const list = await Talk_Comment.findAll({
-        attributes: [
-          "id",
-          "user_email",
-          "comment",
-          "createdAt",
-          [
-            sequelize.literal(
-              "(SELECT COUNT(comment_id) FROM talk_comment_child WHERE comment_id = `Talk_Comment`.id)"
-            ),
-            "child_count",
-          ],
+  try {
+    const user_email = req.session.loginInfo.user_email;
+    const { comment_id, comment, talk_id } = req.body;
+    await Talk_Comment_Child.create({
+      user_email: user_email,
+      comment_id: comment_id,
+      comment: comment,
+    });
+    const list = await Talk_Comment.findAll({
+      attributes: [
+        "id",
+        "user_email",
+        "comment",
+        "createdAt",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(comment_id) FROM talk_comment_child WHERE comment_id = `Talk_Comment`.id)"
+          ),
+          "child_count",
         ],
-        include: [{ model: User, attributes: ["nickname", "user_profile"] }],
-        where: {
-          talk_id,
-        },
-        order: [["created_at", "DESC"]],
-        limit: 10,
-      });
+      ],
+      include: [{ model: User, attributes: ["nickname", "user_profile"] }],
+      where: {
+        talk_id,
+      },
+      order: [["created_at", "DESC"]],
+      limit: 10,
+    });
 
-      const total = await Talk_Comment.count({
-        where: {
-          talk_id: talk_id,
-        },
-      });
+    const total = await Talk_Comment.count({
+      where: {
+        talk_id: talk_id,
+      },
+    });
 
-      let more = false;
-      if (total > 10) more = true;
-      res.json({ list: list, success: 1, more: more, total: total });
-    } catch (error) {
-      console.error(error);
-      res.status(400).json({ success: 3 });
-    }
-  });
+    let more = false;
+    if (total > 10) more = true;
+    res.json({ list: list, success: 1, more: more, total: total });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ success: 3 });
+  }
+});
 
-  // Talk 대댓글 목록 조회
-  router.get("/childList/:comment_id", async (req, res) => {
-    try {
-      const comment_id = req.params.comment_id;
-      const list = await Talk_Comment_Child.findAll({
-        where: {
-          comment_id: comment_id,
-        },
-        order: [["created_at", "DESC"]],
-        include: [{ model: User, attributes: ["nickname", "user_profile"] }],
-      });
-      res.json({ list: list, success: 1 });
-    } catch (error) {
-      res.status(400).json({ success: 3 });
-    }
-  });
-
+// Talk 대댓글 목록 조회
+router.get("/childList/:comment_id", async (req, res) => {
+  try {
+    const comment_id = req.params.comment_id;
+    const list = await Talk_Comment_Child.findAll({
+      where: {
+        comment_id: comment_id,
+      },
+      order: [["created_at", "DESC"]],
+      include: [{ model: User, attributes: ["nickname", "user_profile"] }],
+    });
+    res.json({ list: list, success: 1 });
+  } catch (error) {
+    res.status(400).json({ success: 3 });
+  }
+});
 
 // Talk_Comment 좋아요 등록/삭제
 router.put("/like", async (req, res) => {
   try {
     const { comment_id, status } = req.body;
-    // const { user_email } = req.session.loginInfo;
-    const user_email = "moonnr94@gmail.com";
+    const { user_email } = req.session.loginInfo;
     const history = await Talk_Comment_Like.findOne({
       where: { comment_id, user_email },
     });
 
-
-        if (history) {
-            await Talk_Comment_Like.destroy({
-                where: {comment_id, user_email},
-            });
-        } else {
-            await Talk_Comment_Like.create({
-                comment_id,
-                user_email,
-            });
-        }
-        ;
-
+    if (history) {
+      await Talk_Comment_Like.destroy({
+        where: { comment_id, user_email },
+      });
+    } else {
+      await Talk_Comment_Like.create({
+        comment_id,
+        user_email,
+      });
+    }
     res.json({ success: 1 });
   } catch (error) {
     res.status(400).json({ success: 3 });
